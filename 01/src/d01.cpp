8bd1bba416cfdbf415249ec4c4e004f6b3c06771
Pierre-Luc Gagné <disciplinedsoftware@gmail.com>
2025-12-01 23:33:37 -0500

    Solved d01 second

--- 01/src/d01.cpp
+++ 01/src/d01.cpp
@@ -53,6 +53,45 @@ unsigned long long solve_problem_1(std::string const& filename) {
     return counter;
 }
 
+unsigned long long solve_problem_2(std::string const& filename) {
+    std::filesystem::path const base = "./01/input"s;
+    auto const filepath = base / filename;
+
+    int dial = 50;
+    unsigned long long counter = 0;
+
+    std::ifstream stream(filepath);
+    std::string line;
+    if (stream.fail()) {
+        throw std::runtime_error("Could not open file: " + filepath.string());
+    }
+    while (std::getline(stream, line) || !line.empty()) {
+        std::istringstream line_stream(line);
+        auto steps = std::stoi(line.substr(1));
+
+        counter += steps / 100;
+        steps %= 100;
+
+        if (line[0] == 'L') {
+            dial -= steps;
+        }
+        else if (line[0] == 'R') {
+            dial += steps;
+        }
+        else {
+            throw std::runtime_error("Invalid direction");
+        }
+
+        if (std::abs(dial) != steps && (dial <= 0 || dial >= 100)) {
+            ++counter;
+        }
+
+        dial = (dial + 100) % 100;
+    }
+
+    return counter;
+}
+
 void expect(auto const & result, auto const & reference) {
     if (result == reference) {
         std::cout << "Success" << std::endl;
@@ -63,6 +102,9 @@ void expect(auto const & result, auto const & reference) {
 }
 
 int main() {
-    expect(solve_problem_1("example"s), 3u);
-    std::cout << solve_problem_1("first"s) << std::endl;
+    // expect(solve_problem_1("example"s), 3u);
+    // std::cout << solve_problem_1("first"s) << std::endl;
+
+    expect(solve_problem_2("example"s), 6u);
+    std::cout << solve_problem_2("first"s) << std::endl;
 }
