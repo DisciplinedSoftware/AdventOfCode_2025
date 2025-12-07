87281541a8a75796e25d887fbe7359715d086044
Pierre-Luc Gagné <disciplinedsoftware@gmail.com>
2025-12-07 18:18:00 -0500

    D07 Resolved second

--- 07/src/d07.cpp
+++ 07/src/d07.cpp
@@ -59,6 +59,22 @@ unsigned long long solve_problem_1(std::string const& filename) {
     return result;
 }
 
+/*
+    S    
+    1      1 -> 1
+   1^1     1+1 -> 2
+  1^2^1    1,(1+1),1 -> 4
+  12^3^1   1,2,(2+1),1 -> 7
+ 1^33^4^1  1,(2+1),3,(3+1),1 -> 12
+*/
+
+
+/*
+Keep a count of the number of timelines before
+Add the count when value already exists in the set
+This will keep the list of possibilities shorter
+*/
+/*
 unsigned long long compute_nb_timelines(size_t beam, size_t row, std::vector<std::unordered_set<size_t>> const& splitters) {
     if (row == splitters.size()) {
         return 1;
@@ -104,6 +120,44 @@ unsigned long long solve_problem_2(std::string const& filename) {
     }
 
     return compute_nb_timelines(beam, 0, splitters);
+}
+*/
+
+unsigned long long solve_problem_2(std::string const& filename) {
+    std::filesystem::path const base = "./07/input"s;
+    auto const filepath = base / filename;
+    std::ifstream stream(filepath);
+    if (!stream.is_open()) {
+        throw std::runtime_error("Could not open file: "s + filepath.string());
+    }
+
+    std::string line;
+    if (!std::getline(stream, line)) {
+        throw std::runtime_error("Could not read from file: "s + filepath.string());
+    }
+
+    std::unordered_map<size_t, unsigned long long> beams;
+    beams.reserve(line.size());
+    beams.emplace(line.find('S'), 1);
+    std::unordered_map<size_t, unsigned long long> next_beams;
+    next_beams.reserve(line.size());
+
+    while (std::getline(stream, line) && !line.empty()) {
+        for (auto [beam, count] : beams) {
+            if (line[beam] == '^') {
+                next_beams[beam-1] += count;
+                next_beams[beam+1] += count;
+            }
+            else {
+                next_beams[beam] += count;
+            }
+        }
+
+        std::swap(beams, next_beams);
+        next_beams.clear();
+    }
+
+    return std::ranges::fold_left(beams, 0ull, [](unsigned long long acc, auto const& value) { return acc + value.second; });
 }
 
 void expect(auto const & result, auto const & reference) {
