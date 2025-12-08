cf26c21b2a3ac04a5b36faa081fa15d30c9274f9
Pierre-Luc Gagné <disciplinedsoftware@gmail.com>
2025-12-08 01:05:43 -0500

    D08 Solve first

---
+++ 08/src/d08.cpp
@@ -0,0 +1,163 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <regex>
+#include <set>
+#include <span>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std::string_literals;
+
+std::ifstream open(std::string const & filename) {
+    std::filesystem::path const base = "./08/input"s;
+    auto const filepath = base / filename;
+    std::ifstream stream(filepath);
+    if (!stream.is_open()) {
+        throw std::runtime_error("Could not open file: "s + filepath.string());
+    }
+
+    return stream;
+}
+
+struct Point3D {
+    unsigned long long x;
+    unsigned long long y;
+    unsigned long long z;
+};
+
+namespace std {
+template<>
+struct tuple_size<Point3D> : integral_constant<std::size_t, 3> {};
+
+template<size_t N>
+struct tuple_element<N, Point3D> {
+    using type = unsigned long long;
+};
+}
+
+template<size_t N>
+unsigned long long& get(Point3D& p) {
+    if constexpr (N == 0) return p.x;
+    else if constexpr (N == 1) return p.y;
+    else if constexpr (N == 2) return p.z;
+    else static_assert(N < 3, "Index out of bounds in std::get<Point3D>");
+}
+template<size_t N>
+unsigned long long const& get(Point3D const& p) {
+    if constexpr (N == 0) return p.x;
+    else if constexpr (N == 1) return p.y;
+    else if constexpr (N == 2) return p.z;
+    else static_assert(N < 3, "Index out of bounds in std::get<Point3D>");
+}
+
+unsigned long long solve_problem_1(std::ifstream&& stream, unsigned int nb_shortest_connections) {
+    std::vector<Point3D> connections;
+
+    std::string line;
+    while (std::getline(stream, line)) {
+        std::istringstream line_stream(line);
+        unsigned long long x, y, z;
+        char dummy;
+        line_stream >> x >> dummy >> y >> dummy >> z;
+        connections.emplace_back(x, y, z);
+    }
+
+    std::vector<std::pair<unsigned long long, std::pair<size_t, size_t>>> distances;
+    for (size_t i = 0; i < connections.size(); ++i) {
+        auto const& [x1, y1, z1] = connections[i];
+        for (size_t j = i+1; j < connections.size(); ++j) {
+            auto const& [x2, y2, z2] = connections[j];
+            unsigned long long const dx = x2 - x1;
+            unsigned long long const dy = y2 - y1;
+            unsigned long long const dz = z2 - z1;
+            unsigned long long const distance = dx*dx + dy*dy + dz*dz;
+            distances.emplace_back(distance, std::make_pair(i, j));
+        }
+    }
+
+    std::multimap<size_t, size_t> distance_map;
+    std::ranges::sort(distances);
+    for (unsigned int k = 0; k < nb_shortest_connections && k < distances.size(); ++k) {
+        distance_map.emplace(distances[k].second.first, distances[k].second.second);
+        distance_map.emplace(distances[k].second.second, distances[k].second.first);
+    }
+
+    std::vector<unsigned long long> nb_junction_boxes;
+    std::unordered_set<size_t> visited;
+    size_t sum = 0;
+    for (auto const& [point_index, connected_points] : distance_map) {
+        std::queue<size_t> to_visit;
+        to_visit.push(point_index);
+        auto const [begin, end] = distance_map.equal_range(point_index);
+        while (!to_visit.empty()) {
+            size_t current = to_visit.front();
+            to_visit.pop();
+            if (visited.contains(current)) {
+                continue;
+            }
+            visited.emplace(current);
+            auto const [conn_begin, conn_end] = distance_map.equal_range(current);
+            for (auto it = conn_begin; it != conn_end; ++it) {
+                if (!visited.contains(it->second)) {
+                    to_visit.push(it->second);
+                }
+            }
+        }
+        nb_junction_boxes.emplace_back(visited.size() - sum);
+        sum = visited.size();
+    }
+
+    std::ranges::sort(nb_junction_boxes, std::greater<>{});
+    return std::ranges::fold_left(nb_junction_boxes.begin(), nb_junction_boxes.begin()+3, 1ULL, std::multiplies<>{});
+}
+
+unsigned long long solve_problem_1(std::string const& filename, unsigned int nb_shortest_connections) {
+    return solve_problem_1(open(filename), nb_shortest_connections);
+}
+
+unsigned long long solve_problem_2(std::ifstream&& stream) {
+    unsigned long long result = 0;
+
+    std::string line;
+    while (std::getline(stream, line)) {
+        std::istringstream line_stream(line);
+        
+    }
+
+    return result;
+}
+
+unsigned long long solve_problem_2(std::string const& filename) {
+    return solve_problem_2(open(filename));
+}
+
+void expect(auto const & result, auto const & reference) {
+    if (result == reference) {
+        std::cout << "Success" << std::endl;
+    }
+    else {
+        std::cout << "Failure: " << result << " != " << reference << std::endl;
+    }
+}
+
+int main() {
+    expect(solve_problem_1("example"s, 10), 40);
+    std::cout << solve_problem_1("first"s, 1000) << std::endl;
+
+    // expect(solve_problem_2("example"s), 25272);
+    // std::cout << solve_problem_2("first"s) << std::endl;
+}
