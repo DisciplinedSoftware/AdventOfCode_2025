2f895ceb37840399ad453d50fbdb94f7bfbb2925
Pierre-Luc Gagné <disciplinedsoftware@gmail.com>
2025-12-11 00:23:20 -0500

    D10 second too slow

--- 10/src/d10.cpp
+++ 10/src/d10.cpp
@@ -39,7 +39,6 @@ unsigned long long solve_problem_1(std::ifstream&& stream) {
     std::string line;
     while (std::getline(stream, line)) {
         // [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-        // TODO: use bitsets instead of vector<bool>
         std::bitset<32> lights_final_state;
         std::vector<std::bitset<32>> buttons;
 
@@ -119,8 +118,92 @@ unsigned long long solve_problem_2(std::ifstream&& stream) {
 
     std::string line;
     while (std::getline(stream, line)) {
-        std::istringstream line_stream(line);
-        
+        // [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+
+        std::vector<unsigned int> joltage_final_state;
+        std::vector<std::vector<unsigned int>> buttons;
+
+        size_t i = 1;
+        while (i < line.size()) {
+            if (line[i] == '{') {
+                ++i;
+                while (i < line.size() && line[i] != '}') {
+                    size_t light_index = 0;
+                    while (i < line.size() && std::isdigit(line[i])) {
+                        light_index = light_index * 10 + (line[i] - '0');
+                        ++i;
+                    }
+                    joltage_final_state.emplace_back(light_index);
+                    if (i < line.size() && line[i] == ',') {
+                        ++i;
+                    }
+                }
+                ++i; // skip the closing '}'
+                break;
+            }
+            else if (line[i] == '(') {
+                ++i;
+                std::vector<unsigned int> button_joltages;
+                while (i < line.size() && line[i] != ')') {
+                    size_t light_index = 0;
+                    while (i < line.size() && std::isdigit(line[i])) {
+                        light_index = light_index * 10 + (line[i] - '0');
+                        ++i;
+                    }
+                    button_joltages.emplace_back(light_index);
+                    if (i < line.size() && line[i] == ',') {
+                        ++i;
+                    }
+                }
+                buttons.emplace_back(button_joltages);
+            }
+
+            ++i;
+        }
+
+        auto const max_joltage = *std::max_element(joltage_final_state.begin(), joltage_final_state.end());
+        auto const bump = max_joltage + 1;
+        auto hash_fct = [bump](std::vector<unsigned int> const& vec) {
+            size_t seed = 0;
+            for(auto const i : vec) {
+                seed += seed * bump + i;
+            }
+            return seed;
+        };
+
+        std::unordered_set<std::vector<unsigned int>, decltype(hash_fct)> visited_states(0, hash_fct);
+        // Use bfs to find the minimum number of presses to turn on all lights_final_state
+        std::queue<std::pair<std::vector<unsigned int>, unsigned long long>> bfs_queue;
+
+        bfs_queue.emplace(std::vector<unsigned int>(joltage_final_state.size(), 0), 0);
+        visited_states.emplace(std::vector<unsigned int>(joltage_final_state.size(), 0));
+        while (!bfs_queue.empty()) {
+            auto [joltage_current_state, nb_buttons_pressed] = bfs_queue.front();
+            bfs_queue.pop();
+
+            if (joltage_current_state == joltage_final_state) {
+                result += nb_buttons_pressed;
+                break;
+            }
+
+            for (size_t i = 0; i <buttons.size(); ++i) {
+                auto joltage_next_state = joltage_current_state;
+                bool valid = true;
+                for (auto const j : buttons[i]) {
+                    joltage_next_state[j] += 1;
+                    if (joltage_next_state[j] > joltage_final_state[j]) {
+                        joltage_next_state = joltage_current_state; // invalid state
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (valid && !visited_states.contains(joltage_next_state)) {
+                    visited_states.emplace(joltage_next_state);
+                    bfs_queue.emplace(joltage_next_state, nb_buttons_pressed + 1);
+                }
+            }
+        }
     }
 
     return result;
@@ -143,6 +226,6 @@ int main() {
     expect(solve_problem_1("example"s), 7);
     std::cout << solve_problem_1("first"s) << std::endl;
 
-    // expect(solve_problem_2("example"s), );
-    // std::cout << solve_problem_2("first"s) << std::endl;
+    expect(solve_problem_2("example"s), 33);
+    std::cout << solve_problem_2("first"s) << std::endl;
 }
