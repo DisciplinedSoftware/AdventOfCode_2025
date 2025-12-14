f4012970e47f344113083c1815b98dd2d1518ecb
Pierre-Luc Gagné <disciplinedsoftware@gmail.com>
2025-12-14 01:24:09 -0500

    D11 improved implemenation

--- 11/src/d11.cpp
+++ 11/src/d11.cpp
@@ -63,7 +63,7 @@ unsigned long long solve_problem_1(std::unordered_multimap<id, id>&& devices) {
     id constexpr start = {'y', 'o', 'u'};
     id constexpr finish  = {'o', 'u', 't'};
 
-    auto visit = [&devices, &finish](this auto&& self, id const& current, std::unordered_set<id> const& visited) -> unsigned long long {
+    auto visit = [&devices](this auto&& self, id const& current, id const& finish, std::unordered_set<id>& visited) -> unsigned long long {
         if (current == finish) {
             return 1ULL;
         }
@@ -72,21 +72,22 @@ unsigned long long solve_problem_1(std::unordered_multimap<id, id>&& devices) {
         return std::ranges::fold_left(
             std::ranges::subrange(begin, end),
             0ULL,
-            [&self, &visited](unsigned long long acc, auto const& pair) -> unsigned long long {
+            [&self, &finish, &visited](unsigned long long acc, auto const& pair) -> unsigned long long {
                 if (visited.contains(pair.second)) {
                     return acc;
                 }
-                auto new_visited = visited;
-                new_visited.insert(pair.second);
-                return acc + self(pair.second, new_visited);
+                visited.emplace(pair.second);
+                auto result = acc + self(pair.second, finish, visited);
+                visited.erase(pair.second);
+                return result;
             }
         );
     };
 
     std::unordered_set<id> visited;
-    visited.insert(start);
+    visited.emplace(start);
 
-    return visit(start, visited);
+    return visit(start, finish, visited);
 }
 
 unsigned long long solve_problem_1(std::string const& filename) {
@@ -94,52 +95,81 @@ unsigned long long solve_problem_1(std::string const& filename) {
 }
 
 unsigned long long solve_problem_2(std::unordered_multimap<id, id>&& devices) {
-    id constexpr start = {'s', 'v', 'r'};
-    id constexpr finish  = {'o', 'u', 't'};
+    // The problem is to find all paths from:
+    // - svr to dac (1)
+    // - svr to fft (2)
+    // - dac to fft (3)
+    // - fft to dac (4)
+    // - fft to out (5)
+    // - dac to out (6)
+
+    // Then multiply the number of paths
+    // 1*3*5 + 2*4*6
+
+    id constexpr svr = {'s', 'v', 'r'};
+    id constexpr out  = {'o', 'u', 't'};
 
     id constexpr dac = {'d', 'a', 'c'};
     id constexpr fft = {'f', 'f', 't'};
 
-    auto visit = [&devices, &finish, &dac, &fft](
-            this auto&& self,
-            id const& current,
-            std::bitset<2> const& special_visited,
-            std::unordered_set<id> const& visited) -> unsigned long long {
+    auto visit = [&devices](this auto&& self,
+                            id const& current,
+                            id const& finish,
+                            std::unordered_set<id>& visited,
+                            std::unordered_map<id, unsigned long long>& memoize) -> unsigned long long {
         if (current == finish) {
-            return special_visited.all() ? 1ULL : 0ULL;
+            unsigned long long result = 1ULL;
+            memoize.emplace(current, result);
+            return result;
         }
 
         auto [begin, end] = devices.equal_range(current);
         return std::ranges::fold_left(
             std::ranges::subrange(begin, end),
             0ULL,
-            [&self, &special_visited, &visited, &dac, &fft](
-                    unsigned long long acc,
-                    auto const& pair) -> unsigned long long {
+            [&self, finish, &visited, &memoize](unsigned long long acc, auto const& pair) -> unsigned long long {
                 if (visited.contains(pair.second)) {
                     return acc;
                 }
-
-                auto new_special_visited = special_visited;
-                if (pair.second == dac) {
-                    new_special_visited.set(0);
-                }
-                else if (pair.second == fft) {
-                    new_special_visited.set(1);
-                }
-
-                auto new_visited = visited;
-                new_visited.insert(pair.second);
-                return acc + self(pair.second, new_special_visited, new_visited);
+                visited.emplace(pair.second);
+                auto result = acc + self(pair.second, finish, visited, memoize);
+                visited.erase(pair.second);
+                return result;
             }
         );
     };
 
+    std::unordered_map<id, unsigned long long> memoize;
     std::unordered_set<id> visited;
-    visited.insert(start);
-    std::bitset<2> special_visited;
+    visited.insert(svr);
+    auto svr_dac = visit(svr, dac, visited, memoize);
+    visited.clear();
+    memoize.clear();
 
-    return visit(start, special_visited, visited);
+    visited.insert(svr);
+    auto svr_fft = visit(svr, fft, visited, memoize);
+    visited.clear();
+    memoize.clear();
+
+    visited.insert(dac);
+    auto dac_fft = visit(dac, fft, visited, memoize);
+    visited.clear();
+    memoize.clear();
+
+    visited.insert(fft);
+    auto fft_dac = visit(fft, dac, visited, memoize);
+    visited.clear();
+    memoize.clear();
+
+    visited.insert(fft);
+    auto fft_out = visit(fft, out, visited, memoize);
+    visited.clear();
+    memoize.clear();
+
+    visited.insert(dac);
+    auto dac_out = visit(dac, out, visited, memoize);
+
+    return svr_dac * dac_fft * fft_out + svr_fft * fft_dac * dac_out;
 }
 
 unsigned long long solve_problem_2(std::string const& filename) {
